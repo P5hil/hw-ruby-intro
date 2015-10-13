8236f1ef401ceae047d81e15e980606fb47986dd
p5hil <phil.robinson@tesco.net>
2015-10-13 09:03:49 +0000

    hw0 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,48 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.sort.reverse.take(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  arr.combination(2).count { |a,b| a + b == n } > 0
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if !s or s==""
+  (('a'..'z').to_a - ['a', 'e' 'i', 'o', 'u']).include?(s[0].downcase)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if !s or s=="" or !!s.index(/[^0|1]/)
+  s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+   attr_accessor :isbn , :price
+   #attr_accessor :price
+   def initialize(isbn, price)
+     raise ArgumentError if price <= 0 or isbn.empty?
+     @price = price
+     @isbn = isbn
+   end
+   
+   def price_as_string
+     "$%0.2f" % price
+   end
 end
